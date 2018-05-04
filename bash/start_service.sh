dc331cef5a95cc241fc0ba154afe0707e6e36609
tauwoo <1152876197@qq.com>
2018-05-04 15:48:21 +0800

    修改执行

--- bash/start_service.sh
+++ bash/start_service.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # 拉取数据
-REPO_ROOT = "/home/ubuntu/service/spider_monitor_api"
-cd $REPO_ROOT
+cd /home/ubuntu/service/spider_monitor_api
 git pull origin master
 
 # Kill Process
 ps -ef | grep api\_main\.py | awk {'print $2'} | xargs kill -s 9
-ps -ef | grep hook\_.py | awk {'print $2'} | xargs kill -s 9
+ps -ef | grep hook\.py | awk {'print $2'} | xargs kill -s 9
 
 # Start Process
-nohup python3 ./api_main.py >> api_main.log &
-nohup python3 ./extra/hook.py >> hook.log &
+python3 ./api_main.py >> api_main.log
+python3 ./extra/hook.py >> hook.log
