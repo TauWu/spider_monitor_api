6d46a21309f4e47ff6530a3fdd751c916b3f2666
tauwoo <1152876197@qq.com>
2018-05-04 15:52:20 +0800

    添加hook对应的shell

--- bash/start_service.sh
+++ bash/start_service.sh
@@ -9,5 +9,5 @@ ps -ef | grep api\_main\.py | awk {'print $2'} | xargs kill -s 9
 ps -ef | grep hook\.py | awk {'print $2'} | xargs kill -s 9
 
 # Start Process
-python3 ./api_main.py >> api_main.log
-python3 ./extra/hook.py >> hook.log
+python3 ./api_main.py >> api_main.log &
+python3 ./extra/hook.py >> hook.log &
