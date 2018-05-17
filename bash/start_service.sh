3b920a67c9199d37c39dbb77bb7673e02038710f
TauWoo <tauwoo@seuxw.cc>
2018-05-17 17:02:51 +0800

    构建脚本更新

--- bash/start_service.sh
+++ bash/start_service.sh
@@ -9,5 +9,5 @@ ps -ef | grep api\_main\.py | awk {'print $2'} | xargs kill -s 9
 # ps -ef | grep hook\.py | awk {'print $2'} | xargs kill -s 9
 
 # Start Process
-nohup /usr/bin/python3 ./api_main.py >> api_main.log &
+nohup /usr/bin/python3 ./api_main.py >> /home/ubuntu/service/spider_monitor_api/api_main.log &
 # (python3 ./extra/hook.py >> hook.log) &
